b255ab3b5940bc9aebcce4efbf8c3713b57dc751
SpertuzG <sebastianpertuzg@gmail.com>
2023-08-22 20:41:55 -0500

    Creación de clase Base

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    //Agregar propiedades y métodos comúnes...
+
+}
